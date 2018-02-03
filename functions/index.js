506bb2bc7da9ee4aace4616997aec8de8e3fd65b
Aaron Pulver <apulver@esri.com>
2018-02-02 21:50:14 -0500

    remove unfinished code

--- functions/index.js
+++ functions/index.js
@@ -34,24 +34,3 @@ exports.challenges = functions.https.onRequest((request, response) => {
       response.send(snapshot.val() || {});
   });
 });
-
-exports.standings = functions.https.onRequest((request, response) => {
-  var db = admin.database();
-  var arenaid = request.query.arenaid;
-
-  if (!arenaid){
-    return response.send({"error": "You must specify an arena"})
-  }
-
-  return admin.database().ref('games').orderByChild('arenaid').equalTo(arenaid).once('value', (snapshot) => {
-    var games = snapshot.val() || {};
-    var users = [];
-    Object.keys(games).forEach(key => {
-      var game = games[key];
-      // if player 1 not in list of users add them and their score
-      // if player 2 not in list of users add them and their score
-      // figure out if player 1 or player 2 won, then increment their total wins/loses
-    });
-    return response.send(snapshot || {});
-  });
-});
